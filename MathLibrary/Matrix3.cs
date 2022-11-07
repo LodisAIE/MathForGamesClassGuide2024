8b2859c779f9c9ff05f9f6a9d1b51fe10415f11e
Lodis <lodis.perkins@aie.edu>
2022-11-07 14:24:44 -0600

    Added matrix transform function definitions

--- MathLibrary/Matrix3.cs
+++ MathLibrary/Matrix3.cs
@@ -42,6 +42,36 @@ namespace MathLibrary
             }
         }
 
+        /// <summary>
+        /// Creates a new matrix that has been rotated by the given value in radians.
+        /// </summary>
+        /// <param name="radians">The angle in radians to rotate by.</param>
+        public static Matrix3 CreateRotation(float radians)
+        {
+            return new Matrix3();
+        }
+
+        /// <summary>
+        /// Creates a new matrix that has been translated by the given values.
+        /// </summary>
+        /// <param name="x">The x position of the new matrix.</param>
+        /// <param name="y">The y position of the new matrix.</param>
+        public static Matrix3 CreateTranslation(float x, float y)
+        {
+            return new Matrix3();
+        }
+
+        /// <summary>
+        /// Creates a new matrix that has been scaled by the given values.
+        /// </summary>
+        /// <param name="x">The value to use to scale the matrix in the x axis.</param>
+        /// <param name="y">The value to use to scale the matrix in the y axis.</param>
+        /// <returns>The result of the scale.</returns>
+        public static Matrix3 CreateScale(float x, float y)
+        {
+            return new Matrix3();
+        }
+
         public static Matrix3 operator +(Matrix3 lhs, Matrix3 rhs)
         {
             return new Matrix3();
